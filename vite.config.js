9480cdfb098aff9a55eccc7286f1dca610e44fce
Facu Jackson <facujackson@Mac.home>
2026-07-04 17:48:36 -0300

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Alquileres_Damfield-2.0/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
