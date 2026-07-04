4e399da0b639b02bf8c5dbe80755b1cd4b4d1bc8
Facu Jackson <facujackson@Mac.home>
2026-07-04 19:23:23 -0300

    Add cloud storage integration with Supabase, fix base path for Surge, enable data persistence

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { installCloudStorage } from './cloudStorage';
 
-if (!window.storage) {
-  window.storage = {
-    get: async () => null,
-    set: async () => {},
-  };
-}
+installCloudStorage();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
